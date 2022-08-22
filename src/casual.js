6a157ae46eace3cf3897732bd813351c27d73c86
Marius <marius@example.com>
2022-08-22 11:44:15 +0300

    makeMeCards

--- src/casual.js
+++ src/casual.js
@@ -11,3 +11,23 @@ const city1 = casual.city;
 console.log('city1 ===', city1);
 
 // parasyti funkcija kuri grazina ir atspausdina random varda ir pavarde is casual
+function printRandomName() {
+  const randNameSurname = `${casual.first_name} ${casual.last_name}`;
+  console.log(randNameSurname);
+  return randNameSurname;
+}
+printRandomName();
+
+// parasyti funkcija kuri grazina argumentu paduota skaiciu sugeneruotu mokejimo korteliu
+// makeMeCards(4)
+function makeMeCards(howMany) {
+  const cards = [];
+  for (let i = 0; i < howMany; i++) {
+    cards.push(casual.card_data);
+  }
+  return cards;
+}
+console.log('makeMeCards(4) ===', makeMeCards(4));
+
+// parasyti funkcija kuri sugeneruoja random data metai, menuo, diena. atspausdina ir grazina
+// panaudoti sita funckija ir gauti random data app.js
